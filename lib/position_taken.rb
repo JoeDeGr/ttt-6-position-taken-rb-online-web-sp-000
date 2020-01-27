55d8473c3f67767a0ff18633da102791cc6cb3f7
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-01-27 21:20:47 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,9 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   if board[index] != " " || ""
-true
-elsif board[index] == " " || ""
-  false
-else
+    true
+  elsif board[index] == " " || ""
+    false
+  else
 end
 end
